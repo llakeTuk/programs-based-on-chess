e61c5ab9d86a3e1dcf9590bcd44334da1fbecf00
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-06-20 19:01:09 +0300

    Add files via upload
    Return matrix with possible movements of bishop for inputting coordinate

---
+++ chess_bishop.py
@@ -0,0 +1,20 @@
+coordinate = ''
+def draw(coordinate):
+  coordinate = input("enter coordinate(in format 'b3'): ")
+  letter = 'abcdefgh'
+  y = letter.find(coordinate[0])
+  x = int(coordinate[1]) - 1
+  chessboard = [['.'] * 8 for _ in range(8)]
+  for i in range(8):
+    for j in range(8):
+      if (abs(y - j) == abs(x - i)):
+        chessboard[i][j] = '*'
+  chessboard[x][y] = 'B'
+  for i in range(8):
+    print((i + 1), *chessboard[i])
+  print('  a b c d e f g h')
+y = 'y'
+while y == 'y':
+  draw(coordinate)
+  y = input("Do you want to restart?(y/n): ", )
+print('Goodbye')
