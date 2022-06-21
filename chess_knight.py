4b8e138d789d1a792b5aaf5c6bb4ca29c2ca2635
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-06-21 14:58:13 +0300

    Add files via upload

---
+++ chess_knight.py
@@ -0,0 +1,20 @@
+coordinate = ''
+def draw(coordinate):
+  coordinate = input("enter coordinate(in format 'b3'): ")
+  letter = 'abcdefgh'
+  y = letter.find(coordinate[0])
+  x = 8 - int(coordinate[1])
+  chessboard = [['.'] * 8 for _ in range(8)]
+  for i in range(8):
+    for j in range(8):
+      if ((abs(y - j) == 2) and (abs(x - i) == 1)) or ((abs(y - j) == 1) and (abs(x - i) == 2)):
+        chessboard[i][j] = '*'
+  chessboard[x][y] = 'N'
+  for i in range(8):
+    print((8 - i), *chessboard[i])
+  print('  a b c d e f g h')
+y = 'y'
+while y == 'y':
+  draw(coordinate)
+  y = input("Do you want to restart?(y/n): ", )
+print('Goodbye')
